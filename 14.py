8756f3d551f219a1f8811c881793ff1d76e34583
gabrielpatrick71 <gabrielpvf11@gmail.com>
2022-10-30 14:21:29 -0300

    Atividades da aula do diego

---
+++ 14.py
@@ -0,0 +1,9 @@
+n1 = int(input("Insira o primeiro numero: "))
+n2 = int(input("Insira o segundo numero: "))
+
+dividendo = n1
+divisor = n2
+quociente = dividendo/divisor
+resto = dividendo % divisor
+
+print("Dividendo e {} divisor e {}  quociente e {} resto e {}".format(dividendo, divisor, quociente, resto) )
---
+++ 22.py
@@ -0,0 +1,8 @@
+n = float(input("Digite o número em pés: "))
+polegadas = n * 12 
+jardas = n * 3 
+milhas = jardas / 1.760
+
+print("O valor em polegadas é: ", polegadas)
+print("O valor em jardas é: ", jardas)
+print("O valor em milhas é: ", milhas)

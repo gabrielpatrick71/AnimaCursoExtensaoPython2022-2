b0a165ecc19aa56aed55a86c175dc951f536cac5
Gabriel Ferreira <116230322+gabrielpatrick71@users.noreply.github.com>
2022-11-09 21:36:28 -0300

    contas em py

---
+++ conta1.py
@@ -0,0 +1,8 @@
+x = input("Digite o valor de x: ")
+y = input("Digite o valor de y: ")
+
+resultadox = (x + x)
+resultadoyx = (x + y) + (y + x)
+print("O valor de x é =" + x)
+print("O valor de x + x é = {} ".format (resultadox) )
+print("O valor de (x+y) + (y+x) é = {}".format (resultadoyx))
